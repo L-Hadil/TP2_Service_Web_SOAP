337de3f8c3ae381ac1c66649621e4ee7f3ef3100
L-hadil <hadilladj@gmail.com>
2024-11-03 22:19:03 +0100

    first commit

---
+++ exo2_distributed/src/main/java/org/example/tp2/q2/exo2_distributed/Exo2DistributedApplication.java
@@ -0,0 +1,13 @@
+package org.example.tp2.q2.exo2_distributed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exo2DistributedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exo2DistributedApplication.class, args);
+	}
+
+}
---
+++ exo2_distributed/src/test/java/org/example/tp2/q2/exo2_distributed/Exo2DistributedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tp2.q2.exo2_distributed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exo2DistributedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
